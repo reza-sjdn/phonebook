a118cfeec5a69cc4a560f5d0e7ad52b153e99433
reza-sjdn <rezasajjadian22@gmail.com>
2022-12-29 15:49:10 +0330

    Contact Class

---
+++ contact.cpp
@@ -0,0 +1,83 @@
+// Contact Class Implementation
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include "contact.h"
+
+using namespace std;
+
+ostream& operator<< (ostream& output, const Contact& contact) {
+    output << "#" << left << setw(5) << contact.getID() << setw(15) <<
+        contact.getFName() << setw(25) << contact.getLName() <<
+        setw(12) << contact.getPhnNum() << endl;
+    output << "------------------------------------------------------------------------------------\n";
+    return output;
+}
+
+
+istream& operator>> (istream& input, Contact& contact) {
+    cout << "Enter First Name: ";
+    string fName;
+    input >> fName;
+    contact.setFName(fName);
+
+    cout << "Enter Last Name: ";
+    string lName;
+    input >> lName;
+    contact.setLName(lName);
+
+    cout << "Enter Phone Number: ";
+    string phnNum;
+    input >> phnNum;
+    contact.setPhnNum(phnNum);
+
+    return input;
+}
+
+
+Contact::Contact (int contactID, const string& firstName, const string& lastName, const string& phoneNumber) {
+    setID(contactID);
+    setFName(firstName);
+    setLName(lastName);
+    setPhnNum(phoneNumber);
+}
+
+Contact::~Contact () {}
+
+
+void Contact::setID (int id) {
+    if (id >= 0 && id <= 200) this->id = id;
+    else cout << "not a valid id\n";
+}
+
+int Contact::getID () const {return id;}
+
+
+void Contact::setFName (const string& fName) {
+    size_t length{fName.size()};
+    length = (length < 15 ? length : 14);
+    fName.copy(this->fName, length);
+    this->fName[length] = '\0';
+}
+
+const char* Contact::getFName () const {return fName;}
+
+
+void Contact::setLName (const string& lName) {
+    size_t length{lName.size()};
+    length = (length < 15 ? length : 14);
+    lName.copy(this->lName, length);
+    this->lName[length] = '\0';
+}
+
+const char* Contact::getLName () const {return lName;}
+
+
+void Contact::setPhnNum (const string& phnNum) {
+    size_t length{phnNum.size()};
+    length = (length < 15 ? length : 14);
+    phnNum.copy(this->phnNum, length);
+    this->phnNum[length] = '\0';
+}
+
+const char* Contact::getPhnNum () const {return phnNum;}
---
+++ contact.h
@@ -0,0 +1,34 @@
+// Contact Class Interface
+#ifndef CONTACT_H
+#define CONTACT_H
+#include <iostream>
+#include <string>
+
+class Contact {
+    friend std::ostream& operator<< (std::ostream&, const Contact&);
+    friend std::istream& operator>> (std::istream&, Contact&);
+public:
+    Contact (int = 0, const std::string& = "", const std::string& = "",
+        const std::string& = "");
+    ~Contact();
+    
+    void setID (int);
+    int getID () const;
+
+    void setFName (const std::string&);
+    const char* getFName () const;
+
+    void setLName (const std::string&);
+    const char* getLName () const;
+
+    void setPhnNum (const std::string&);
+    const char* getPhnNum () const;
+
+private:
+    int id;
+    char fName[15];
+    char lName[25];
+    char phnNum[12];
+};
+
+#endif
